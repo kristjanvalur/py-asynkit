71cfe61ab4cbdcd2799799fd156b11b8204d511e
Kristján Valur Jónsson <sweskman@gmail.com>
2022-04-09 16:18:10 +0000

    more tests

--- src/async_df.py
+++ src/async_df.py
@@ -3,6 +3,17 @@ import asyncio
 from asyncio import events
 
 
+if sys.version_info[1] == 7:
+
+    def _start_task(coro, loop, name):
+        return asyncio.Task(coro, loop=loop)
+
+else:
+
+    def _start_task(coro, loop, name):
+        return asyncio.Task(coro, loop=loop, name=name)
+
+
 class EventLoopMixin:
     """
     A mixin class adding features to the base event loop.
@@ -10,14 +21,13 @@ class EventLoopMixin:
 
     def rotate_ready(self, n: int):
         """Rotate the ready queue.
-        
+
         The leftmost part of the ready queue is the callback called nex.
-        
+
         A negative value will rotate the queue to the left, placing the next entry at the end.
         A Positive values will move callbacks from the end to the front, making them next in line.
         """
         self._ready.rotate(n)
-
 
     def call_insert(self, position, callback, *args, context=None):
         """Arrange for a callback to be inserted at `position` in the queue to be
@@ -32,7 +42,7 @@ class EventLoopMixin:
         self._check_closed()
         if self._debug:
             self._check_thread()
-            self._check_callback(callback, 'call_insert')
+            self._check_callback(callback, "call_insert")
         handle = events.Handle(callback, args, self, context)
         if handle._source_traceback:
             del handle._source_traceback[-1]
@@ -43,25 +53,26 @@ class EventLoopMixin:
 class DepthFirstSelectorEventLoop(asyncio.SelectorEventLoop, EventLoopMixin):
     pass
 
+
 if hasattr(asyncio, "ProactorEventLoop"):
+
     class DepthFirstProactorEventLoop(asyncio.ProactorEventLoop, EventLoopMixin):
         pass
 
 
 class DepthFirstEventLoopPolicy(asyncio.DefaultEventLoopPolicy):
-    if sys.platform == 'win32':
+    if sys.platform == "win32":
         _loop_factory = DepthFirstProactorEventLoop
     else:
         _loop_factory = DepthFirstSelectorEventLoop
 
 
-
 async def sleep_insert(pos, result=None):
     """Coroutine that completes after `pos` other callbacks have been run.
-    
+
     This effectively pauses the current coroutine and places it at position `pos`
     in the ready queue.
-    
+
     In case the current event loop doesn't support the `call_insert` method, it
     behaves the same as sleep(0)
     """
@@ -70,25 +81,23 @@ async def sleep_insert(pos, result=None):
         call_insert = loop.call_insert
     except AttributeError:
         return await asyncio.sleep(0, result)
-    
+
     future = loop.create_future()
-    h = call_insert(pos,
-                        futures._set_result_unless_cancelled,
-                        future, result)
+    h = call_insert(pos, futures._set_result_unless_cancelled, future, result)
     try:
         return await future
     finally:
         h.cancel()
 
 
-async def start(coro, *, loop=None, name=None):
+async def descend(coro, *, loop=None, name=None):
     """Starts the coroutine immediately.
     The current coroutine is paused, to be resumed immediatelly when the called coroutine
     first pauses.  A future is returned.
     """
 
     loop = loop or asyncio.get_event_loop()
-    task = asyncio.Task(coro, loop=loop, name=name)
+    task = _start_task(coro, loop=loop, name=name)
     try:
         # the task was previously at the end.  Make it the next runnable task
         loop.rotate_ready(1)
@@ -99,3 +108,13 @@ async def start(coro, *, loop=None, name=None):
         await asyncio.sleep(0)
     return task
 
+
+async def start(coro, *, loop=None, name=None):
+    """Starts the coroutine _soon_.
+    A Task is created and the current coroutine
+    **yields** allowing the other to commence.
+    """
+    loop = loop or asyncio.get_event_loop()
+    task = _start_task(coro, loop, name)
+    await asyncio.sleep(0)
+    return task
--- tests/test_df.py
+++ tests/test_df.py
@@ -1,16 +1,32 @@
 import asyncio
 import async_df
 
-async def dummy_request(result="dummy_request", log=[], delay=0.01):
+
+async def request(result="request", log=[], delay=0.01):
     log.append(result)
     await asyncio.sleep(delay)
     log.append(result)
     return result
 
 
+async def two_requests(result="two_request", log=[], delay=0.01):
+    log.append(result)
+    r1 = request(result="r1", log=log)
+    r2 = request(result="r2", log=log, delay=0.02)
+
+
 async def test_normal_simple():
     log = []
-    future = dummy_request(log=log)
+    future = request(log=log)
     log.append(1)
     await future
-    assert log == [1, "dummy_request", "dummy_request"]
+    assert log == [1, "request", "request"]
+
+
+async def test_start_simple():
+    log = []
+    future = request(log=log)
+    future = await async_df.start(future)
+    log.append(1)
+    await future
+    assert log == ["request", 1, "request"]
