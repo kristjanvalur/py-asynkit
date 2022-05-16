e8764ae94bb0be534c93909e9963dae7c7046db8
Kristján Valur Jónsson <sweskman@gmail.com>
2022-05-16 13:10:01 +0000

    Add an argument to select the Proactor loop for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,21 @@
 import pytest
-from asynkit import DefaultSchedulingEventLoop
+import asynkit
+
+SelectorLoop = asynkit.SchedulingSelectorEventLoop
+SchedulingLoop = getattr(asynkit, "SchedulingProactorEventLoop")
+
+
+def pytest_addoption(parser):
+    parser.addoption("--proactor", action="store_true", default=False)
+
 
 @pytest.fixture
-def event_loop():
-    loop = DefaultSchedulingEventLoop()
-    yield loop
-    loop.close()
+def event_loop(request):
+    loop_type = SelectorLoop
+    if SchedulingLoop and request.config.getoption("proactor"):
+        loop_type = SchedulingLoop
+    loop = loop_type()
+    try:
+        yield loop
+    finally:
+        loop.close()
