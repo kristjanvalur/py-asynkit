02a3b6e564f41231c2384a77774d166f379cc830
Kristján Valur Jónsson <sweskman@gmail.com>
2022-12-21 12:12:44 +0000

    Proper ipmlementation of Monitor and Generator

--- src/asynkit/susp.py
+++ src/asynkit/susp.py
@@ -1,31 +1,46 @@
-
-from typing import Coroutine, Tuple, Any, Optional
 import types
+from typing import Any, Coroutine, Optional, Tuple
 
-class Suspender:
-    __slots__ = ["coro", "suspended"]
-    
-    def __init__(self):
-        self.coro: Optional[Coroutine] = None
-        self.suspended = False
+from .coroutine import coro_is_finished
 
-    async def call(self, coroutine: Coroutine) -> Tuple[bool, Any]:
+
+class Monitor:
+    __slots__ = [
+        "coro",
+        "is_oob",
+    ]
+
+    def __init__(self, coroutine: Optional[Coroutine] = None):
         self.coro = coroutine
-        return await self.resume(None)
-        
+        self.is_oob = False
+
+    def init(self, coroutine: Coroutine):
+        self.coro = coroutine
+        return self
+
     @types.coroutine
-    def resume(self, message: Optional[Any] = None) -> Tuple[bool, Any]:
+    def oob_await(self, message: Optional[Any] = None) -> Tuple[bool, Any]:
+        """
+        Await with oob (Out Of Band data)
+        returns a tuple `(is_oob, data)` where if `is_oob` is true, `data`
+        is the data passed to the `oob()` method.
+        Otherwise, it is the result of awaiting the function
+        """
+
         try:
             out_value = self.coro.send(message)
         except StopIteration as exc:
-            return (True, exc.value)
+            return (False, exc.value)
+        return (yield from self.oob_continue(out_value))
 
+    @types.coroutine
+    def oob_continue(self, out_value):
         # yield up the values
         # This is similar to how `yield from` is defined (see pep-380)
         # except that it uses a coroutines's send() and throw() methods.
         while True:
-            if self.suspended:
-                return (False, out_value)        
+            if self.is_oob:
+                return (True, out_value)
             try:
                 in_value = yield out_value
             except GeneratorExit:  # pragma: no coverage
@@ -36,20 +51,64 @@ class Suspender:
                 try:
                     out_value = self.coro.throw(exc)
                 except StopIteration as exc:
-                    return (True, exc.value)
+                    return (False, exc.value)
             else:
                 try:
                     out_value = self.coro.send(in_value)
                 except StopIteration as exc:
-                    return (True, exc.value)
-    
+                    return (False, exc.value)
 
     @types.coroutine
-    def suspend(self, message: Optional[Any] = None) -> Any:
-        assert not self.suspended
-        self.suspended = True
+    def oob(self, message: Optional[Any] = None) -> Any:
+        """
+        Send Out Of Band data to a higher up caller which is using `oob_await()`
+        """
+        assert not self.is_oob
+        self.is_oob = True
         try:
             return (yield message)
         finally:
-            self.suspended = False
-        
+            self.is_oob = False
+
+
+class Generator(Monitor):
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        return await self.asend(None)
+
+    async def asend(self, value):
+        if coro_is_finished(self.coro):
+            raise StopAsyncIteration()
+        is_oob, result = await self.oob_await(value)
+        if not is_oob:
+            self.exited = True
+            raise StopAsyncIteration()
+        return result
+
+    async def athrow(self, type, value=None, traceback=None):
+        if coro_is_finished(self.coro):
+            return None
+        is_oob, result = await self.oob_throw(type, value, traceback)
+        if not is_oob:
+            self.exited = True
+            raise StopAsyncIteration()
+        return result
+
+    async def aclose(self):
+        await self.athrow(GeneratorExit)
+
+    async def ayield(self, value: Any) -> Any:
+        """
+        Asynchronously yield the value to the Generator object
+        """
+        return await self.oob(value)
+
+    @types.coroutine
+    def oob_throw(self, type, value=None, traceback=None):
+        try:
+            out_value = self.coro.throw(type, value, traceback)
+        except StopIteration as exc:
+            return (False, exc.value)
+        return (yield from self.oob_continue(out_value))
--- tests/test_susp.py
+++ tests/test_susp.py
@@ -1,26 +1,148 @@
-
-from asynkit.susp import Suspender
 import asyncio
 
+import pytest
 
-async def test_suspend():
+from asynkit.susp import Generator, Monitor
 
-    async def helper(s):
-        back = await s.suspend("foo")
+
+async def test_monitor():
+    async def helper(m):
+        back = await m.oob("foo")
         await asyncio.sleep(0)
-        back = await s.suspend(back + "foo")
+        back = await m.oob(back + "foo")
         await asyncio.sleep(0)
-        return back+"foo"
+        return back + "foo"
 
-    s = Suspender()
-    finished, back = await s.call(helper(s))
-    assert not finished
+    m = Monitor()
+    is_oob, back = await m.init(helper(m)).oob_await()
+    assert is_oob
     assert back == "foo"
-    finished, back = await s.resume("hoo")
-    assert not finished
+    is_oob, back = await m.oob_await("hoo")
+    assert is_oob
     assert back == "hoofoo"
-    finished, back = await s.resume("how")
-    assert finished
+    is_oob, back = await m.oob_await("how")
+    assert not is_oob
     assert back == "howfoo"
-    
-    
+
+
+async def top(g):
+    v = await bottom(g, 10)
+    await asyncio.sleep(0)
+    return await bottom(g, 20, v)
+
+
+async def bottom(g, i, v=None):
+    v = await g.ayield(i + 1 + (v or 0))
+    await asyncio.sleep(0)
+    v = await g.ayield(i + 2 + (v or 0))
+    await asyncio.sleep(0)
+    v = await g.ayield(i + 3 + (v or 0))
+    await asyncio.sleep(0)
+    return v
+
+
+async def topfail(g):
+    v = await bottomfail(g, 10)
+    await asyncio.sleep(0)
+    return await bottomfail(g, 20, v)
+
+
+async def bottomfail(g, i, v=None):
+    v = await g.ayield(i + 1 + (v or 0))
+    await asyncio.sleep(0)
+    raise ZeroDivisionError()
+    v = await g.ayield(i + 2 + (v or 0))
+    await asyncio.sleep(0)
+    v = await g.ayield(i + 3 + (v or 0))
+    await asyncio.sleep(0)
+    return v
+
+
+async def standard():
+    v = 0
+    for i in [11, 12, 13, 21, 22, 23]:
+        v = yield i + (v or 0)
+
+
+async def standardfail():
+    yield 11
+    raise ZeroDivisionError()
+    v = 0
+    for i in [12, 13, 21, 22, 23]:
+        v = yield i + (v or 0)
+
+
+async def assert_exhausted(g):
+    with pytest.raises(StopAsyncIteration):
+        r = await g.asend(None)
+    with pytest.raises(StopAsyncIteration):
+        r = await g.__anext__()
+    # but throw just works (weird)
+    r = await g.athrow(EOFError)
+    assert r is None
+
+
+@pytest.mark.parametrize("gen_type", ["std", "oob"], ids=["async gen", "Generator"])
+class TestGenerator:
+    async def test_generator_iter(self, gen_type):
+        g = self.normal(gen_type)
+        results = [f async for f in g]
+        assert results == [11, 12, 13, 21, 22, 23]
+
+    async def test_generator_send(self, gen_type):
+        g = self.normal(gen_type)
+        r = await g.asend(None)
+        assert r == 11
+        r = await g.asend(0)
+        assert r == 12
+        r = await g.asend(100)
+        assert r == 113
+
+    def normal(self, gen_type):
+        if gen_type == "std":
+            return standard()
+        g = Generator()
+        return g.init(top(g))
+
+    def fail(self, gen_type):
+        if gen_type == "std":
+            return standardfail()
+        g = Generator()
+        return g.init(topfail(g))
+
+    async def test_send_to_new(self, gen_type):
+        # test send to new generator
+        g = self.normal(gen_type)
+        with pytest.raises(TypeError):
+            await g.asend("foo")
+        # but it should be iterable
+        assert await g.__anext__() == 11
+
+    async def test_send_exhausted(self, gen_type):
+        # test exhausted generator
+        g = self.normal(gen_type)
+        [f async for f in g]
+        # generator should now be exhausted
+        await assert_exhausted(g)
+
+    async def test_throw(self, gen_type):
+        # test exhaustion after throw
+        g = self.normal(gen_type)
+        with pytest.raises(ZeroDivisionError):
+            await g.athrow(ZeroDivisionError)
+        await assert_exhausted(g)
+
+    async def test_throw_live(self, gen_type):
+        # test exhaustion after throw while live
+        g = self.normal(gen_type)
+        await g.__anext__()
+        with pytest.raises(ZeroDivisionError):
+            await g.athrow(ZeroDivisionError)
+        await assert_exhausted(g)
+
+    async def test_fail(self, gen_type):
+        # test generator which throws an error
+        g = self.fail(gen_type)
+        with pytest.raises(ZeroDivisionError):
+            [f async for f in g]
+        await assert_exhausted(g)
