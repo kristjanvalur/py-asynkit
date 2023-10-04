4cd263529c65dea37c983363d753e89059d84e1c
Kristján Valur Jónsson <sweskman@gmail.com>
2023-10-04 11:01:13 +0000

    remove accidental checkin

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,10 +1,8 @@
-import asyncio
 from collections import deque
 
 import pytest
 
 import asynkit.tools
-from asynkit.loop.extensions import get_scheduling_loop
 
 from .conftest import SchedulingEventLoopPolicy
 
@@ -38,38 +36,3 @@ def test_deque_pop():
         else:
             with pytest.raises(IndexError):
                 asynkit.tools.deque_pop(deq, i)
-
-
-async def test_get_wakeup():
-    # create an event, then make a task which waits on it
-    # then, get the wakeup callback from the task
-    # and store it
-
-    loop = get_scheduling_loop()
-    event = asyncio.Event()
-
-    # create a fresh task
-    task = asyncio.create_task(event.wait())
-    queue = loop.get_ready_queue()
-    for h in queue:
-        if loop.get_task_from_handle(h) is task:
-            break
-    else:
-        assert False, "task not found in ready queue"
-
-    # create a just awoken task
-    task = asyncio.create_task(event.wait())
-    await asyncio.sleep(0)
-    event.set()
-
-    queue = loop.get_ready_queue()
-    for h in queue:
-        if loop.get_task_from_handle(h) is task:
-            break
-    else:
-        assert False, "task not found in ready queue"
-
-    # now, we have the handle, we can get the wakeup callback
-
-    event.set()
-    await task
