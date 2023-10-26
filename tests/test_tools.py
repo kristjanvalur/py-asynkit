64b3ca66998baaf86820821d03a0f484dadbe662
Kristján Valur Jónsson <sweskman@gmail.com>
2023-10-26 16:04:58 +0000

    add tests for PriorityQueue

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,8 +1,10 @@
+import random
 from collections import deque
 
 import pytest
 
 import asynkit.tools
+from asynkit.tools import PriorityQueue
 
 from .conftest import SchedulingEventLoopPolicy
 
@@ -36,3 +38,176 @@ def test_deque_pop():
         else:
             with pytest.raises(IndexError):
                 asynkit.tools.deque_pop(deq, i)
+
+
+class PriorityObject:
+    def __init__(self, priority):
+        self.priority = priority
+
+    def __repr__(self):
+        return f"PriorityObject({self.priority})"
+
+
+def priority_key(obj):
+    return obj.priority
+
+
+class TestPriorityQueue:
+    def test_append(self):
+        queue = PriorityQueue()
+
+        obj1 = PriorityObject(1)
+        obj2 = PriorityObject(2)
+        obj3 = PriorityObject(3)
+
+        for i in range(5):
+            obj = [obj1, obj2, obj3]
+            random.shuffle(obj)
+            for o in obj:
+                queue.append(o.priority, o)
+
+            assert len(queue) == 3
+            assert queue.popleft() == (1, obj1)
+            assert queue.popleft() == (2, obj2)
+            assert queue.popleft() == (3, obj3)
+            pytest.raises(IndexError, queue.popleft)
+
+    def test_fifo(self):
+        """Verify that same priority gets FIFO ordering"""
+        queue = PriorityQueue()
+
+        obj1 = PriorityObject(1)
+        obj2 = PriorityObject(2)
+        obj3 = PriorityObject(3)
+        obj1a = PriorityObject(1)
+        obj2a = PriorityObject(2)
+        obj3a = PriorityObject(3)
+
+        for i in range(5):
+            assert len(queue) == 0
+            obj = [obj1, obj2, obj3]
+            random.shuffle(obj)
+            for o in obj:
+                queue.append(o.priority, o)
+
+            obj = [obj1a, obj2a, obj3a]
+            random.shuffle(obj)
+            for o in obj:
+                queue.append(o.priority, o)
+
+            assert len(queue) == 6
+            assert queue.popleft() == (1, obj1)
+            assert queue.popleft() == (1, obj1a)
+            assert queue.popleft() == (2, obj2)
+            assert queue.popleft() == (2, obj2a)
+            assert queue.popleft() == (3, obj3)
+            assert queue.popleft() == (3, obj3a)
+            pytest.raises(IndexError, queue.popleft)
+
+    def test_priority_iter(self):
+        """Verify that iteration returns items in priority order"""
+        queue = PriorityQueue()
+
+        obj1 = PriorityObject(1)
+        obj2 = PriorityObject(2)
+        obj3 = PriorityObject(3)
+
+        for i in range(5):
+            assert len(queue) == 0
+            obj = [obj1, obj2, obj3]
+            random.shuffle(obj)
+            for o in obj:
+                queue.append(o.priority, o)
+
+            itered = list(queue)
+            popped = [queue.popleft() for i in range(3)]
+            assert itered == popped
+
+    def test_find(self):
+        """Verify that iteration returns items in priority order"""
+        queue = PriorityQueue()
+
+        obj1 = PriorityObject(1)
+        obj2 = PriorityObject(2)
+        obj3 = PriorityObject(3)
+        obj4 = PriorityObject(4)
+
+        for i in range(3):
+            assert len(queue) == 0
+            obj = [obj1, obj2, obj3, obj4]
+            for i in range(1, 5):
+                obj.append(PriorityObject(i))
+            random.shuffle(obj)
+            for o in obj:
+                queue.append(o.priority, o)
+
+            def getkey(obj):
+                return lambda k: k is obj
+
+            assert queue.find(getkey(obj1)) == (1, obj1)
+            assert queue.find(getkey(obj2)) == (2, obj2)
+            assert queue.find(getkey(obj2), priority_hint=2) == (2, obj2)
+            assert queue.find(getkey(obj2), priority_hint=1) == (2, obj2)
+            assert queue.find(getkey(obj2), priority_hint=7) == (2, obj2)
+            assert queue.find(getkey(obj3), priority_hint=3, remove=True) == (3, obj3)
+            assert queue.find(lambda k: False, priority_hint=2) is None
+
+            assert queue.find(getkey(obj3), remove=True) is None
+
+            itered = list(queue)
+            pris = [o.priority for (pri, o) in itered]
+            expected = [1, 1, 2, 2, 3, 4, 4]
+            assert pris == expected
+            assert queue
+            for pri, obj in itered:
+                assert queue.find(getkey(obj), remove=True) == (obj.priority, obj)
+            assert len(queue) == 0
+            assert not queue
+
+    def test_reschedule(self):
+        """Verify that iteration returns items in priority order"""
+        queue = PriorityQueue()
+
+        obj1 = PriorityObject(1)
+        obj2 = PriorityObject(2)
+        obj3 = PriorityObject(3)
+        obj4 = PriorityObject(4)
+
+        for i in range(3):
+            queue.clear()
+            assert len(queue) == 0
+            obj = [obj1, obj2, obj3, obj4]
+            random.shuffle(obj)
+            for o in obj:
+                queue.append(o.priority, o)
+            for o in obj:
+                queue.append(o.priority, PriorityObject(o.priority))
+
+            def getkey(obj):
+                return lambda k: k is obj
+
+            objs = list(queue)
+            assert objs[4][1] is obj3
+
+            queue.reschedule(getkey(obj3), 7)
+            objs = list(queue)
+            assert objs[-1][1] is obj3
+
+            found = queue.reschedule(getkey(obj3), -1, priority_hint=7)
+            assert found == obj3
+            objs = list(queue)
+            assert objs[0][1] is obj3
+
+            queue.reschedule(getkey(obj3), 3, priority_hint=7)
+            objs = list(queue)
+            assert objs[5][1] is obj3  # now at end of its priority deque
+            pris = [o.priority for (pri, o) in objs]
+            assert pris == [1, 1, 2, 2, 3, 3, 4, 4]
+
+            # reschedule to same priority
+            queue.reschedule(getkey(obj3), 3, priority_hint=7)
+
+            # pop first value and try to reschedule a popped value
+            pri, obj = queue.popleft()
+            found = queue.reschedule(getkey(obj), 3, priority_hint=7)
+            assert found is None
